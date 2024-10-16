bbc4f1ce5be14cf491c83ea8b0388dc392383b7f
alecbeck <alecbecklin@gmail.com>
2024-10-15 22:22:13 -0500

    Covert Text nodes with bold, code or italic markdown

--- src/converter.py
+++ src/converter.py
@@ -34,5 +34,23 @@ def text_node_to_html_node(text_node: TextNode):
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-	# this will split md text into nodes
-	#old_nodes is is list
+	if not isinstance(text_type, TextType):
+		raise ValueError("text_type must be an instance of TextType Enum")
+	new_nodes = []
+	#loop throught the old_nodes list
+	for node in old_nodes:
+		if node.text_type == TextType.TEXT and delimiter in node.text:
+			#split the node text
+			texts = node.text.split(delimiter)
+			if len(texts) % 2 == 0:
+				raise Exception(f"missing closeing {delimiter} in {node.text}")
+			for i, txt in enumerate(texts):
+				if txt:
+					if i % 2 == 0:
+						new_nodes.append(TextNode(txt, TextType.TEXT))
+					else:
+						new_nodes.append(TextNode(txt, text_type))
+		else:
+			new_nodes.append(node)
+	return new_nodes
+
